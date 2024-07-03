d18556a92e1fc18e898f2b45f777e7199fb8bc30
brandon-toy <brandontoy@uvic.ca>
2024-07-02 17:23:52 -0700

    remove vim code browse

--- init.lua
+++ init.lua
@@ -661,15 +661,6 @@ require('lazy').setup({
     end,
   },
 
-  -- Vim code browse (internal amazon tool)
-
-  {
-    url = 'ssh://git.amazon.com:2222/pkg/Vim-code-browse',
-    branch = 'mainline',
-    dependencies = 'tpope/vim-fugitive',
-    event = 'VeryLazy',
-  },
-
   { -- Autoformat
     'stevearc/conform.nvim',
     lazy = false,
