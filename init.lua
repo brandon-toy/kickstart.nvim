dfcbecc9ee6c8001bbba9abbc45784aa8dca6928
Brandon Toy <bghtoy@amazon.com>
2024-07-04 11:00:14 -0700

    add custom setups

--- init.lua
+++ init.lua
@@ -164,8 +164,8 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz', { desc = 'Go to next quickfix menu' })
-vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz', { desc = 'Go to prev quickfix menu' })
+vim.keymap.set({ 'n', 'v' }, '<leader>j', '<cmd>cnext<CR>', { desc = 'Go to next quickfix menu' })
+vim.keymap.set({ 'n', 'v' }, '<leader>k', '<cmd>cprev<CR>', { desc = 'Go to prev quickfix menu' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
