789bdba7ee78c2fcdf32295244b401964d16eca2
Brandon Toy <bghtoy@amazon.com>
2024-07-31 11:38:09 -0700

    work

--- init.lua
+++ init.lua
@@ -286,20 +286,25 @@ require('lazy').setup({
     config = function() -- This is the function that runs, AFTER loading
       require('which-key').setup()
 
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      local wk = require 'which-key'
+
+      wk.add {
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>c_', hidden = true },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>d_', hidden = true },
+        { '<leader>h', group = 'Git [H]unk' },
+        { '<leader>h_', hidden = true },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>r_', hidden = true },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>s_', hidden = true },
+        { '<leader>t', group = '[T]oggle' },
+        { '<leader>t_', hidden = true },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>w_', hidden = true },
+        { '<leader>h', desc = 'Git [H]unk', mode = 'v' },
       }
-      -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
     end,
   },
 
@@ -664,6 +669,31 @@ require('lazy').setup({
           end,
         },
       }
+
+      local lspconfig = require 'lspconfig'
+      local configs = require 'lspconfig.configs'
+
+      if not configs.codewhisperer then
+        configs.codewhisperer = {
+          default_config = {
+            -- Add the codewhisperer to our PATH or BIN folder
+            cmd = { 'cwls' },
+            root_dir = lspconfig.util.root_pattern('packageInfo', 'package.json', 'tsconfig.json', 'jsconfig.json', '.git'),
+            filetypes = { 'java', 'python', 'typescript', 'javascript', 'csharp', 'ruby', 'kotlin', 'shell', 'sql', 'c', 'cpp', 'go', 'rust', 'lua' },
+          },
+        }
+      end
+      if not configs.codewhisperer then
+        configs.codewhisperer = {
+          default_config = {
+            -- Add the codewhisperer to our PATH or BIN folder
+            cmd = { 'cwls' },
+            root_dir = lspconfig.util.root_pattern('packageInfo', 'package.json', 'tsconfig.json', 'jsconfig.json', '.git'),
+            filetypes = { 'java', 'python', 'typescript', 'javascript', 'csharp', 'ruby', 'kotlin', 'shell', 'sql', 'c', 'cpp', 'go', 'rust', 'lua' },
+          },
+        }
+      end
+      lspconfig.codewhisperer.setup {}
     end,
   },
 
@@ -981,6 +1011,14 @@ require('lazy').setup({
           },
         },
       }
+    end,
+  },
+  {
+    'MeanderingProgrammer/markdown.nvim',
+    name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    config = function()
+      require('render-markdown').setup {}
     end,
   },
 }, {
